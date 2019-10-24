52a901a4a1f4ebfe90c0c605f9e3c356dd5aa326
TimzOwen <ktimothy@kabarak.ac.ke>
2019-10-24 05:52:13 +0300

    completed

--- Downloads/Compressed/candy/Android-CandyCodedApplication-master/app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ Downloads/Compressed/candy/Android-CandyCodedApplication-master/app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void addCandiesToDatabase(Candy[] candies) {
+    private void addCandiesToDatabase(Candy[] candies) {,.
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
         for (Candy candy : candies) {
